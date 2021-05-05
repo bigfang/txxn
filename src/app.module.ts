eed0bd40c7181b77428d773316430cd3d941187d
bigfang <bitair@gmail.com>
2021-05-05 22:21:48 +0800

    add users module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,12 +2,14 @@ import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [
     GraphQLModule.forRoot({
-      autoSchemaFile: 'schema.gql',
+      autoSchemaFile: true,
     }),
+    UsersModule,
   ],
   controllers: [AppController],
   providers: [AppService],
