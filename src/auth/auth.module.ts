12085fe442bc8e2dcfc1a55d5e1acf81b7376e53
bigfang <bitair@gmail.com>
2021-06-10 01:18:51 +0800

    add login resolver

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -2,12 +2,13 @@ import { Module } from '@nestjs/common';
 import { JwtModule } from '@nestjs/jwt';
 import { PassportModule } from '@nestjs/passport';
 import { UserModule } from 'src/user/user.module';
+import { AuthResolver } from './auth.resolver';
 import { AuthService } from './auth.service';
 import { JwtStrategy } from './strategies/jwt.strategy';
 import { LocalStrategy } from './strategies/local.strategy';
 
 @Module({
-  providers: [AuthService, LocalStrategy, JwtStrategy],
+  providers: [AuthResolver, AuthService, LocalStrategy, JwtStrategy],
   imports: [
     UserModule,
     PassportModule,
---
+++ src/auth/auth.resolver.ts
@@ -0,0 +1,16 @@
+import { Args, Query, Resolver } from '@nestjs/graphql';
+import { Public } from 'src/common/decorators/public.decorator';
+import { AuthService } from './auth.service';
+import { LoginArgs } from './dto/login.args';
+import { LoginResult } from './entities/login.entity';
+
+@Resolver(() => LoginResult)
+export class AuthResolver {
+  constructor(private readonly authService: AuthService) {}
+
+  @Public()
+  @Query(() => LoginResult)
+  login(@Args() loginArgs: LoginArgs) {
+    return this.authService.login(loginArgs);
+  }
+}
